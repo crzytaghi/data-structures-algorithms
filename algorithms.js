e9d7b1f04a9cf166a6cf365ed3003cf9c543b3e1
Andrew Taghi <aataghi13@gmail.com>
2020-09-11 17:32:28 -0500

    updated code organization

--- algorithms.js
+++ algorithms.js
@@ -313,20 +313,20 @@
 // ========== FLATLAND SPACE STATIONS ========== //
 // Determine the max distance any city is from a space station given n (the lenght of the array) & c (the values at which the space station(s) is/are located)
 
-let flatLandSpaceStations = (n, c) => {
-
-  let distances = [];
-    c.sort((a,b)=>a-b);
-
-    for (let i = 0; i < c.length-1; i++) {
-        const dist = Math.floor(Math.abs(c[i + 1] - c[i]) / 2)
-        distances.push(dist)
-    }
-
-    //find max distances before and after
-    const before = Math.abs(c[0] - 0)
-    const after = Math.abs(n - c[c.length - 1])-1
-    return Math.max(...distances,before,after)
-}
-
-console.log(flatLandSpaceStations(5,[0,4]));
+// let flatLandSpaceStations = (n, c) => {
+//
+//   let distances = [];
+//     c.sort((a,b)=>a-b);
+//
+//     for (let i = 0; i < c.length-1; i++) {
+//         const dist = Math.floor(Math.abs(c[i + 1] - c[i]) / 2)
+//         distances.push(dist)
+//     }
+//
+//     //find max distances before and after
+//     const before = Math.abs(c[0] - 0)
+//     const after = Math.abs(n - c[c.length - 1])-1
+//     return Math.max(...distances,before,after)
+// }
+//
+// console.log(flatLandSpaceStations(5,[0,4]));
